a290142917819ada3a8ca53ab481bccd01eef2ef
José Augusto <79456742+josaugusto@users.noreply.github.com>
2021-02-22 11:45:51 -0300

    Add files via upload

---
+++ Agenda-telefonica(listas)/Cabeçalhos.h
@@ -0,0 +1,6 @@
+void adicionar_contato(void);
+void alterar_contato(struct Agenda *lista);
+void excluir_contato(struct Agenda *lista);
+void salvar_Agenda(struct Agenda  *lista);
+void localizar_contato(struct Agenda  *lista);
+void listar_contatos(struct Agenda  *lista);
